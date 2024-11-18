6dc12e3b2843eaf9f7ff4a565d4daeab6edd0a2d
Kevin Bahl <168040763+KevinBahl07@users.noreply.github.com>
2024-11-17 21:12:14 -0300

    Create Auto.ts

---
+++ Auto.ts
@@ -0,0 +1,55 @@
+import { Rueda } from "./Rueda";
+
+export class Auto {
+    //propiedades
+    private marca: string;
+    private modelo: string;
+    private ruedas: Rueda[];
+    private patente : string;
+
+    //constructor
+    constructor(marca: string, modelo: string, ruedas: Rueda[], patente : string) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ruedas = ruedas;
+        this.patente = patente;
+    }
+
+    // metodos
+    
+    //getters
+    public getMarca(): string {
+        return this.marca;
+    }
+    public getModelo(): string {
+        return this.modelo;
+    }
+    public getRuedas(): number[] {
+        return this.ruedas.map(rueda => rueda.getTamañoRueda());
+    }
+    public getPatente(): string{
+        return this.patente;
+    }
+
+    //setters
+    public setMarca(marca : string): void {
+        if(marca != undefined && marca != ""){
+            this.marca = marca;
+        }
+    }
+    public setModelo(modelo : string): void{
+        if(modelo != undefined && modelo != ""){
+            this.modelo = modelo;
+        }
+    }
+    public setRuedas(ruedas : Rueda[]): void{
+        if(ruedas != undefined && ruedas.length >= 4){
+            this.ruedas = ruedas;
+        }
+    }
+    public setPatente(patente : string): void{
+        if(patente != undefined && patente.length >= 6){
+            this.patente = patente;
+        }
+    }
+}
