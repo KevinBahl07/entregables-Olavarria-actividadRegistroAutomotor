c9ba7b1433dfbe1f566329f82fd956e06bf80ad3
Kevin Bahl <168040763+KevinBahl07@users.noreply.github.com>
2024-11-17 21:21:33 -0300

    Create Direccion.ts

---
+++ Direccion.ts
@@ -0,0 +1,14 @@
+export class Direccion {
+    //propiedades
+    private direccion: number;
+
+    //constructor
+    public constructor(sistema direccion: number) {
+        this.direccion = sistema de frenos;
+    }
+
+    //metodos
+    public getsuspension(): number {
+        return this.direccion;
+    }
+}
